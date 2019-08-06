003c98f9fb278ac775f736b0d8ebf107fe5f5320
Dmitriy Kovalev <kodimweb@gmail.com>
2019-08-06 19:18:53 +0300

    html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 jQuery(document).ready(function ($) {
-
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 });
